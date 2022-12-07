0f68591f4d5c2db85300caacb3d4ded42bc7a698
Vasiliy <vasenka_kuznetsov_1997@mail.ru>
2022-12-07 22:16:29 +0300

    Fix_code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        if (account >= 1) {
-            System.out.println("На вашем счету: " + account + " руб.");
-        } else {
-            System.out.println("На вашем счету: " + account + " руб.");
-        }
-        int replenishment = 5000;
-        System.out.println("Вы пополнили счет на: " + replenishment + " руб.");
+        int replenishment = 1100;
         int bonus;
-        if (replenishment >= 1000) {
+        if (replenishment > 1099) {
             bonus = replenishment / 100;
         } else {
             bonus = 0;
         }
+        System.out.println("На вашем счету: " + account + " руб.");
+        System.out.println("Вы пополнили счет на: " + replenishment + " руб.");
         System.out.println("Вам зачислен бонус: " + bonus + " руб.");
         System.out.println("Ваш счет составлявет: " + (account + replenishment + bonus) + " руб.");
     }
